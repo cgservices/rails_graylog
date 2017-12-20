99fa59a2dc57b61d529aacd89b2a8d2fea50457f
Jeroen Ubbink <jeroen@blackbyte.nl>
2017-12-20 11:20:24 +0100

    remove exception handler, it should already be available

--- lib/rails_graylog/amqp_notifier.rb
+++ lib/rails_graylog/amqp_notifier.rb
@@ -1,17 +1,14 @@
 module RailsGraylog
   class AmqpNotifier
-    attr_reader :exchange, :exception_handler
+    attr_reader :exchange
 
-    def initialize(channel:, exchange_name: 'logging', exception_handler:, **exchange_args)
-      @exception_handler = exception_handler
+    def initialize(channel:, exchange_name: 'logging', **exchange_args)
       raise 'No connection available' unless channel
       @exchange = channel.exchange(exchange_name, exchange_args)
     end
 
     def notify!(message, routing_key = 'log_write')
       exchange.publish(gelf_message.merge(message).to_json, routing_key: routing_key)
-    rescue => e
-      exception_handler.call e
     end
 
     private
